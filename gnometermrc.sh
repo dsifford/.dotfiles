7047703b25e2d47361106786e23a0aa0789bc1cc
Derek P Sifford <dereksifford@gmail.com>
2016-02-10 16:46:00 -0500

    Update terminal colors + make borderless

--- gnometermrc.sh
+++ gnometermrc.sh
@@ -4,8 +4,6 @@ PROFILE_ID="$(dconf list /org/gnome/terminal/legacy/profiles:/)"
 DCONF_PATH="/org/gnome/terminal/legacy/profiles:/${PROFILE_ID}"
 
 
-dconf write ${DCONF_PATH}foreground-color			"'rgb(211,215,207)'"
-dconf write ${DCONF_PATH}background-color 			"'rgb(0,48,81)'"
 dconf write ${DCONF_PATH}use-system-font			"false"
 dconf write ${DCONF_PATH}cursor-shape     			"'block'"
 dconf write ${DCONF_PATH}use-theme-colors 			"false"
@@ -16,22 +14,11 @@ dconf write ${DCONF_PATH}bold-color-same-as-fg 			"true"
 dconf write ${DCONF_PATH}audible-bell 				"false"
 # NOTE: The palate is colored starting with top-left and proceeding rightward
 # 	ending at the bottom right
+
+# Theme: Material (www.terminal.sexy)
+dconf write ${DCONF_PATH}foreground-color "'#eceff1'"
+dconf write ${DCONF_PATH}background-color "'#263238'"
 dconf write ${DCONF_PATH}palette \
-	"[
-		'rgb(78,78,78)',
-		'rgb(255,107,96)',
-		'rgb(250,176,54)',
-		'rgb(255,255,182)',
-		'rgb(86,150,237)',
-		'rgb(255,115,253)',
-		'rgb(142,228,120)',
-		'rgb(238,238,238)',
-		'rgb(79,79,79)',
-		'rgb(249,104,96)',
-		'rgb(250,176,54)',
-		'rgb(253,255,184)',
-		'rgb(107,159,237)',
-		'rgb(252,110,249)',
-		'rgb(142,228,120)',
-		'rgb(255,255,255)'
-	]"
+"
+['#263238', '#ff9800', '#8bc34a', '#ffc107', '#03a9f4', '#e91e63', '#009688', '#cfd8dc', '#37474f', '#ffa74d', '#9ccc65', '#ffa000', '#81d4fa', '#ad1457', '#26a69a', '#eceff1']
+"
