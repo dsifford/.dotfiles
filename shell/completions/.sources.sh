206e2949581d382cfc3bfb9135ace8986e828813
Derek P Sifford <dereksifford@gmail.com>
2017-06-30 12:31:11 -0400

    various updates + remove completion files from version control

--- shell/completions/.sources.sh
+++ shell/completions/.sources.sh
@@ -19,7 +19,7 @@ get_completions() {
         [docker-compose]='https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose'
         [docker-machine]='https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine.bash'
         [exercism]='http://cli.exercism.io/exercism_completion.bash'
-        [docker]='https://raw.githubusercontent.com/moby/moby/master/contrib/completion/bash/docker'
+        [docker]='https://raw.githubusercontent.com/docker/cli/master/contrib/completion/bash/docker'
         [git-prompt]='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
         [git]='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash'
         [hub]='https://raw.githubusercontent.com/github/hub/master/etc/hub.bash_completion.sh'
