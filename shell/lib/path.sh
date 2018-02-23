4d4067208801d9032866b5668033762edc5a5a17
Derek P Sifford <dereksifford@gmail.com>
2018-02-23 18:51:30 -0500

    add completion aliases

--- shell/lib/path.sh
+++ shell/lib/path.sh
@@ -1,40 +1,40 @@
 # shellcheck shell=bash
 
 path_append() {
-    declare -p path > /dev/null || exit 1
-    declare value="$2"
-    declare item
+	declare -p path >/dev/null || exit 1
+	declare value="$2"
+	declare item
 
-    for item in "${path[@]}"; do
-        [ "$item" == "$value" ] && return
-    done
+	for item in "${path[@]}"; do
+		[ "$item" == "$value" ] && return
+	done
 
-    path+=( "$value" )
+	path+=("$value")
 }
 
 parse_path() {
-    declare output
-    declare item
-    declare -a path=(
-        ~/bin
-        ~/.yarn-global/bin
-        ~/.cargo/bin
-        ~/.local/bin
-        ~/gocode/bin
-        /usr/local/opt/coreutils/libexec/gnubin
-        /usr/local/bin
-        /usr/local/sbin
-        /usr/local/go/bin
-    )
+	declare output
+	declare item
+	declare -a path=(
+		~/bin
+		~/.yarn-global/bin
+		~/.cargo/bin
+		~/.local/bin
+		~/gocode/bin
+		/usr/local/opt/coreutils/libexec/gnubin
+		/usr/local/bin
+		/usr/local/sbin
+		/usr/local/go/bin
+	)
 
-    mapfile -d: -t -C path_append -c 1 <<< "${PATH}$( getconf PATH )"
+	mapfile -d: -t -C path_append -c 1 <<<"${PATH}$(getconf PATH)"
 
-    for item in "${path[@]}"; do
-        [[ ! -d "$item" ]] && continue
-        output+="$( [ -z "$output" ] && echo -n "$item" || echo -n ":$item" )"
-    done
+	for item in "${path[@]}"; do
+		[[ ! -d "$item" ]] && continue
+		output+="$([ -z "$output" ] && echo -n "$item" || echo -n ":$item")"
+	done
 
-    echo "$output"
+	echo "$output"
 }
 
 PATH="$(parse_path)"
