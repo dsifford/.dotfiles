6c5035d9c32964263dc0d5cfa464a1e6be463857
Derek Sifford <dereksifford@gmail.com>
2019-05-05 21:36:25 -0400

    misc updates

--- config/ranger/plugins/plugin_file_filter.py
+++ config/ranger/plugins/plugin_file_filter.py
@@ -1,18 +1,19 @@
 # This plugin improves the default hidden file filter
 # by respecting .gitignore if one exists
 from __future__ import absolute_import, division, print_function
+
 from logging import getLogger
-from os import getenv, path, getcwd
-from subprocess import run, PIPE, CalledProcessError
-from typing import Optional, List
+from subprocess import PIPE, CalledProcessError, run
+from typing import Callable, Dict, List, Optional
 
-from ranger.container.fsobject import FileSystemObject  # pyre-ignore
-import ranger.container.directory  # pyre-ignore
+import ranger.container.directory
+from ranger.container.fsobject import FileSystemObject
 
-log = getLogger(__file__)
-ignored_cache = {}
+FilterList = List[Callable[[FileSystemObject], bool]]
 
-accept_file_core = ranger.container.directory.accept_file
+BASE_ACCEPT_FILE = ranger.container.directory.accept_file
+IGNORED_CACHE: Dict[str, Optional[List[str]]] = {}
+LOGGER = getLogger(__file__)
 
 
 def get_ignored_files(curr_path: str) -> Optional[List[str]]:
@@ -34,22 +35,23 @@ def get_ignored_files(curr_path: str) -> Optional[List[str]]:
         return [] if err.returncode == 1 else None
 
 
-def accept_file(fobj: FileSystemObject, filters: list):
-    filter_hidden_active = next(
-        (i for i in filters if i.__name__ is "hidden_filter_func"), None
-    )
-    if not filter_hidden_active:
-        return True
+def filter_gitignore(fobj: FileSystemObject) -> bool:
     workdir = fobj.dirname
     filename = fobj.basename
-    if workdir in ignored_cache:
-        ignored = ignored_cache[workdir]
+    if workdir in IGNORED_CACHE:
+        ignored = IGNORED_CACHE[workdir]
     else:
         ignored = get_ignored_files(fobj.dirname)
-        ignored_cache[workdir] = ignored
+        IGNORED_CACHE[workdir] = ignored
     if ignored is not None:
-        return False if filename in [*ignored, ".git"] else True
-    return accept_file_core(fobj, filters)
+        return not filename in [*ignored, ".git"]
+    return True
+
+
+def accept_file(fobj: FileSystemObject, filters: FilterList) -> bool:
+    if not fobj.fm.settings.show_hidden and filter_gitignore(fobj) is False:
+        return False
+    return BASE_ACCEPT_FILE(fobj, filters)
 
 
 ranger.container.directory.accept_file = accept_file
