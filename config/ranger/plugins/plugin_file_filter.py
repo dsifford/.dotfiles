e50544070ba925b0dd815e76388358fce2aedf1a
Derek Sifford <dereksifford@gmail.com>
2019-09-11 18:03:39 -0400

    improve ranger file filter in git repositories

--- config/ranger/plugins/plugin_file_filter.py
+++ config/ranger/plugins/plugin_file_filter.py
@@ -1,10 +1,11 @@
-# This plugin improves the default hidden file filter
-# by respecting .gitignore if one exists
-from __future__ import absolute_import, division, print_function
-
+"""
+A ranger plugin for improving the default hidden file filter by
+respecting .gitignore in git repositories.
+"""
+from functools import lru_cache
 from logging import getLogger
-from subprocess import PIPE, CalledProcessError, run
-from typing import Callable, Dict, List, Optional
+from subprocess import DEVNULL, CalledProcessError, check_call, check_output  # nosec
+from typing import AnyStr, Callable, List
 
 import ranger.container.directory
 from ranger.container.fsobject import FileSystemObject
@@ -12,45 +13,61 @@ from ranger.container.fsobject import FileSystemObject
 FilterList = List[Callable[[FileSystemObject], bool]]
 
 BASE_ACCEPT_FILE = ranger.container.directory.accept_file
-IGNORED_CACHE: Dict[str, Optional[List[str]]] = {}
 LOGGER = getLogger(__file__)
 
 
-def get_ignored_files(curr_path: str) -> Optional[List[str]]:
+@lru_cache()
+def get_ignored_files(cwd: str) -> List[str]:
+    """Returns a list of git ignored files in cwd, or None if none exist."""
     try:
-        ignored_files = (
-            run(
-                f"cd {curr_path} && git check-ignore * .[^.]*",
-                check=True,
-                stdout=PIPE,
-                stderr=PIPE,
-                shell=True,
+        return (
+            check_output(
+                "git check-ignore * .[^.]*",
+                cwd=cwd,
+                shell=True,  # nosec
+                stderr=DEVNULL,
+                text=True,
             )
-            .stdout.decode("utf-8")
             .strip()
             .split("\n")
         )
-        return ignored_files
-    except CalledProcessError as err:
-        return [] if err.returncode == 1 else None
+    except CalledProcessError:
+        return []
 
 
-def filter_gitignore(fobj: FileSystemObject) -> bool:
-    workdir = fobj.dirname
-    filename = fobj.basename
-    if workdir in IGNORED_CACHE:
-        ignored = IGNORED_CACHE[workdir]
-    else:
-        ignored = get_ignored_files(fobj.dirname)
-        IGNORED_CACHE[workdir] = ignored
-    if ignored is not None:
-        return not filename in [*ignored, ".git"]
-    return True
+@lru_cache()
+def file_is_inside_git_repo(dirname: AnyStr) -> bool:
+    """Returns True if fobject represents a file inside a git repo."""
+    try:
+        check_call(
+            "git rev-parse --is-inside-work-tree",
+            cwd=dirname,
+            shell=True,  # nosec
+            stderr=DEVNULL,
+            stdout=DEVNULL,
+        )
+        return True
+    except CalledProcessError:
+        return False
+
+
+def file_is_gitignored(fobj: FileSystemObject) -> bool:
+    """Returns whether or not a file is git ignored."""
+    LOGGER.info(get_ignored_files.cache_info())
+    return fobj.basename in [*get_ignored_files(fobj.dirname), ".git"]
 
 
 def accept_file(fobj: FileSystemObject, filters: FilterList) -> bool:
-    if not fobj.fm.settings.show_hidden and filter_gitignore(fobj) is False:
-        return False
+    """Returns True if file shall be shown, otherwise False."""
+    if not fobj.fm.settings.show_hidden and file_is_inside_git_repo(fobj.dirname):
+        return not file_is_gitignored(fobj) and BASE_ACCEPT_FILE(
+            fobj,
+            [
+                f
+                for f in filters
+                if not hasattr(f, "__name__") or f.__name__ != "hidden_filter_func"
+            ],
+        )
     return BASE_ACCEPT_FILE(fobj, filters)
 
 
