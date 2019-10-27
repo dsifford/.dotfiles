ef933d59e89ddcb6f30f83f8654244782916f654
Derek Sifford <dereksifford@gmail.com>
2019-10-26 21:13:43 -0400

    misc updates

---
+++ local/lib/bashrc.d/functions/torrent.sh
@@ -0,0 +1,21 @@
+# shellcheck shell=bash
+#
+# Commands for starting and stopping torrent services
+#
+
+for cmd in transmission-daemon transmission-remote windscribe; do
+	command -v "$cmd" > /dev/null || return
+done
+
+torrent-start() {
+	windscribe connect best
+	windscribe firewall on
+	transmission-daemon
+	$BROWSER https://ipleak.net/
+}
+
+torrent-stop() {
+	transmission-remote --exit
+	windscribe firewall auto
+	windscribe disconnect
+}
